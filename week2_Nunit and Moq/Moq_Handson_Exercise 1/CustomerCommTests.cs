611406296264b90e630c17d485884ce477519484
ayan-xyz <2206172@kiit.ac.in>
2025-06-28 21:07:09 +0530

    Add files via upload

---
+++ week2_Nunit and Moq/Moq_Handson_Exercise 1/CustomerCommTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerCommLib.Tests
+{
+    public class CustomerCommTests
+    {
+        [Test]
+        public void SendMailToCustomer_WhenCalled_ShouldReturnTrue()
+        {
+            // Arrange
+            var mockMailSender = new Mock<IMailSender>();
+            mockMailSender.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            var customerComm = new CustomerComm(mockMailSender.Object);
+
+            // Act
+            var result = customerComm.SendMailToCustomer();
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
