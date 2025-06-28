99fd1744aa31cda4251c749594d1ab020fd2313c
ayan-xyz <2206172@kiit.ac.in>
2025-06-28 21:02:01 +0530

    Add files via upload

---
+++ week2_Nunit and Moq/Nunit_Handson_1/CalculatorTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using CalcLibrary;
+
+namespace calctests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _calculator = null;
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(5, 5, 10)]
+        public void Add_ReturnsCorrectResult(int a, int b, int expected)
+        {
+            var result = _calculator.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
